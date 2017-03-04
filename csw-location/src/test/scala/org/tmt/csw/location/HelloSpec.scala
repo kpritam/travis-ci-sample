f849fcc73ff0cc85f82eaa5cd8b2cf45498ad615
Pritam Kadam <phkadam2008@gmail.com>
2017-03-04 22:46:35 +0530

    Pritam :  travis.

--- csw-location/src/test/scala/org/tmt/csw/location/HelloSpec.scala
+++ csw-location/src/test/scala/org/tmt/csw/location/HelloSpec.scala
@@ -5,6 +5,8 @@ import org.scalatest._
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
+
+    assert(false)
   }
 
 }
