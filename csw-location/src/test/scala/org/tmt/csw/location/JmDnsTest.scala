2170446ddaf708dd788e6ebac4a7060d5647b489
Pritam Kadam <phkadam2008@gmail.com>
2017-03-06 17:49:20 +0530

    Pritam : Integrate travis with docker

---
+++ csw-location/src/test/scala/org/tmt/csw/location/JmDnsTest.scala
@@ -0,0 +1,28 @@
+package org.tmt.csw.location
+
+import javax.jmdns.{JmDNS, ServiceInfo}
+
+import org.scalatest.{FunSuite, Matchers}
+import org.tmt.csw.location.common.Networks
+
+/**
+  * Created by pritamkadam on 3/6/17.
+  */
+class JmDnsTest extends FunSuite with Matchers {
+
+  test("should create jmdns install and register service"){
+    val service = ServiceInfo.create("_tmt._tcp.local.", "tmt3", 516, "")
+    val service1 = ServiceInfo.create("_tmt._tcp.local.", "tmt4", 517, "")
+
+    val jmDNS = JmDNS.create(Networks.getPrimaryIpv4Address)
+
+    jmDNS.registerService(service)
+    jmDNS.registerService(service1)
+
+    jmDNS.getServiceInfo("_tmt._tcp.local.", "tmt3") shouldEqual(ServiceInfo.create("_tmt._tcp.local.", "tmt4", 517, ""))
+
+  }
+
+
+
+}
